cc1cf4c9c703582e086b791b1be31669f34ef7e7
Phillipe Mugnaini <phillipe841@gmail.com>
2026-02-12 10:03:20 -0300

    Projeto ToDo List

--- 11-02/Projeto - ToDo List/scripts/scripts.js
+++ 11-02/Projeto - ToDo List/scripts/scripts.js
@@ -1,8 +1,41 @@
-let task = document.querySelector("#input").value
 
-function inputTask(){
+function addTask(){
+
+    //pega o input
+    let input = document.querySelector("#input")
+    let text = input.value
+
+    if (text === ""){
+        alert("Digite uma tarefa!")
+        return
+    }
+
+    //pega a área onde as tarefas vão aparecer
+    let taskArea = document.querySelector("#taskList")
+
+    //cria uma tarefa (div)
+    let task = document.createElement("li")
+    task.innerText = text
+
+   let taskText = document.createElement("span");
+    taskText.style.cursor = "pointer";
+    taskText.onclick = function() {
+        taskText.classList.toggle("completed");
+    };
 
 
+    deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "X"
+    deleteBtn.classList.add("delete-btn")
 
+    deleteBtn.onclick = function(){
+        task.remove()
+    }
 
-}
+    task.appendChild(taskText)
+    task.appendChild(deleteBtn)
+
+    taskArea.appendChild(task)
+
+    input.value =""
+ }
